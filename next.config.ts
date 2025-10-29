66db1a3d3fe7631e96e9bdc6683dda88084d89f7
Ahmad AL-atharbeh <ahmadal-atharbeh@Ahmads-MacBook-Air.local>
2025-10-30 01:59:14 +0300

    fix next.config and build success

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-    ],
-  },
   devIndicators: {
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
+    position: 'bottom-right',
   },
   reactStrictMode: false,
 };
